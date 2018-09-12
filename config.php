534023160342d5ff1c62eae45e69b63304468eb1
Jeffrey-Lang <34451211+Jeffrey-Lang@users.noreply.github.com>
2018-09-13 00:07:02 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,22 +4,38 @@ define('DB_USERNAME', '');
 define('DB_PASSWORD', '');
 define('DB_NAME', '');
 
+// Cronjob key
+define('KEY', '2136DAasUIt');
+
 // Show data in view user
 define('SHOW_PROPERTIES', true);
 define('SHOW_CAR_LIST', true);
 
 // Set Tables and Columns
 define('USERS_TABLE', 'users');
+define('USERS_TABLE_COLUM_ID', 'id');
+define('USERS_TABLE_COLUM_TIMEPLAYED', 'timeplayed');
+define('USERS_TABLE_COLUM_ONLINE', 'online');
 
+// Banned Table
 define('USERS_BANNED_TABLE', 'ea_bans');
 define('USERS_BANNED_ID_COLUMN', 'banid');
 
+// Kicks table
+define('KICKS_TABLE', 'kicks');
+define('KICKS_COLUMN_STEAMID', 'steamid');
+define('KICKS_COLUMN_REASON', 'reason');
+define('KICKS_COLUMN_KICKED', 'kicked');
+
+// Owned vehicles table
 define('OWNED_VEHICLES_TABLE', 'owned_vehicles');
 define('OWNED_VEHICLES_OWNER_COLUMN', 'owner');
 
+// Inventory Table
 define('TRUNK_INVENTORY_TABLE', 'truck_inventory');
 define('TRUNK_INVENTORY_COLUM_ID', 'id');
 
+// Accounts table
 define('USER_ACCOUNTS_TABLE', 'user_accounts');
 define('USER_ACCOUNTS_IDENTIFIER_COLUMN', 'identifier');
 
